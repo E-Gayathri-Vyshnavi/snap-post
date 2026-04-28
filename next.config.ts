c4a204083fd29d73040539f9992549c5a38cf4e2
E Gayathri Vyshnavi <vyshnaviearthineni@gmail.com>
2026-04-28 19:40:30 +0530

    Add postinstall script for prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@prisma/client"], // 👈 Add this line
 };
 
 export default nextConfig;
